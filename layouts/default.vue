8cb5dbac7ee9829076797b10729669659e27b92c
tomascabrerizo <tomascabrerizo97@gmail.com>
2024-07-18 18:07:27 -0300

    horizontal scroll animation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,58 @@
 
 <template>
     <div ref="main">
-        <div class="comic-panel">
-            <Comic/>
-        </div>
-        <div class="page">
-            <div class="nav">
-                <NuxtLink to="/">Home</NuxtLink>
-                <NuxtLink to="/about">About</NuxtLink>
+        <Spidey />
+        <div class="wrapper">
+            <div class="comic-panel">
+                <Comic/>
             </div>
-            <Spidey />
-            <slot />
+            <div class="page-panel">
+                <div class="nav">
+                    <NuxtLink to="/">Home</NuxtLink>
+                    <NuxtLink to="/about">About</NuxtLink>
+                </div>
+                <slot />
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
 
-    const { $gsap } = useNuxtApp();
+    const { $gsap, $ScrollTrigger } = useNuxtApp();
     const main = ref();
     const ctx = ref();
 
     onMounted(() => {
 
+        ctx.value = $gsap.context((self) => {        
 
-
-        ctx.value = $gsap.context((self) => {
+            let wrapper = $gsap.utils.toArray(".wrapper")[0];
+            let wrapper_width = wrapper.offsetWidth;
+            let amout_to_scroll = wrapper_width - window.innerWidth;
             
-            let comic = $gsap.utils.toArray(".comic-panel")[0];
-            //console.log(comic.offsetHeight);
-            localStorage.setItem('scroll', comic.offsetHeight);
-
-            $gsap.to(comic, {
-                scrollTrigger: {
-                    
-                    trigger: comic,
-                    pin: true,
-                    pinSpacing: false,
-                    scrub: true,
-                    markers: false,
-                    
-                    onEnter: () => {
-                        comic.style.pointerEvents = "none";
-                    },
-                    onLeave: () => {
-                        comic.style.pointerEvents = "auto";
-                    },
-                    onEnterBack: () => {
-                        comic.style.pointerEvents = "none";
-                    },
-                    onLeaveBack: () => {
-                        comic.style.pointerEvents = "auto";
-                    },
-
-                },
-                opacity: 0,
+            let tween = $gsap.timeline();
+            tween.to(wrapper, {
+                x: -amout_to_scroll,
+                duration: 3,
+                ease: "none",
+            });
+            tween.to(wrapper, {
+                duration: 1,
+                ease: "none",
             });
             
+            $ScrollTrigger.create({
+                trigger:wrapper,
+                start: "0% 0%",
+                end: "+=" + amout_to_scroll,
+                pin: true,
+                animation: tween,
+                scrub: 1,
+                markers: true,
+            });
+
+
         }, main.value);
 
 
@@ -70,7 +66,19 @@
 
 <style scoped>
 
+    .wrapper {
+        margin: 0;
+        padding: 0;
+
+        display: flex;
+        flex-direction: row;
+        flex-wrap:no-wrap;
+        overflow:auto;
+        width: 200vw;
+    }
+
     .comic-panel {
+        
         display: flex;
         flex-direction: column;
         justify-content: center;
@@ -80,6 +88,11 @@
         height: 100vh;
 
         background-color: white;
+    }
+
+    .page-panel {
+        width: 100%;
+        min-height: 200vh;
     }
 
     .nav {
@@ -102,10 +115,6 @@
         font-weight: bold;
         color: white;
         text-decoration: none;
-    }
-
-    .page {
-        min-height: 100vh;
     }
 
 </style>
