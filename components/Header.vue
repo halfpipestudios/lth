5be56676a075483f609ffb665e974ca247dbe971
tomascabrerizo <tomascabrerizo97@gmail.com>
2024-08-05 23:42:41 -0300

    always center lemon tree logo

--- components/Header.vue
+++ components/Header.vue
@@ -2,6 +2,7 @@
 
     <header>
         <div class="full">
+
             <div class="nav-container">
                 <NuxtLink to="/">HABITACIONES</NuxtLink>
                 <NuxtLink to="/services">SERVICIOS</NuxtLink>
@@ -14,19 +15,36 @@
             <div class="nav-container">
                 <NuxtLink to="/">CONTACTO</NuxtLink>
                 <img src="/img/reservar.svg" alt="">
+                <div class="language">
+                    <select>
+                        <option value="es">ESP</option>
+                        <option value="en">ENG</option>
+                    </select>
+                </div>
             </div>
+
         </div>
 
         <div class="mobile">
             
             <div class="nav">
-                <img class="hamburger" src="/img/hamburger.svg" alt="">
+                <div class="hamburger">
+                    <img class="open-icon" src="/img/hamburger.svg" alt="hamburger">
+                    <img class="close-icon" src="/img/x.svg" alt="x" style="display: none;">
+                </div>
+
                 <div class="logo-container">
                     <div class="logo-name">LEMON TREE</div>
                     <div class="logo-subname">HOSTEL</div>
                 </div>
-            </div>
 
+                <div class="language">
+                    <select>
+                        <option value="es">ESP</option>
+                        <option value="en">ENG</option>
+                    </select>
+                </div>
+            </div>
 
             <div class="menu">
                 <NuxtLink class="close-menu" to="/">HABITACIONES</NuxtLink>
@@ -47,28 +65,46 @@
 
         var hamburger_is_open = false;
 
-        const menu = document.querySelector(".menu");
-        const hamburger = document.querySelector(".hamburger");
-        
+        const mobile = document.querySelector(".mobile");
+
+        const menu = mobile.querySelector(".menu");
+        const hamburger = mobile.querySelector(".hamburger");
+        const open_icon =  mobile.querySelector(".open-icon");
+        const close_icon =  mobile.querySelector(".close-icon");
+        const language = mobile.querySelector(".language");
+        const logo = mobile.querySelector(".logo-container");
+
+        function open_hamburger_menu() {
+            menu.style.display = "flex";
+            open_icon.style.display = "none";
+            close_icon.style.display = "block";
+            language.style.visibility = "visible";
+            logo.style.visibility = "hidden";
+        }
+
+        function close_hamburger_menu() {
+            menu.style.display = "none";
+            open_icon.style.display = "block";
+            close_icon.style.display = "none";
+            language.style.visibility  = "hidden";
+            logo.style.visibility = "visible";
+        }
+
         hamburger.onclick = () => {
 
             hamburger_is_open = !hamburger_is_open;
 
             if(hamburger_is_open) {
-                menu.style.display = "flex";
+                open_hamburger_menu();
             } else {
-                menu.style.display = "none";
-                
+                close_hamburger_menu();
             }
 
         }
 
         const menu_items = document.getElementsByClassName("close-menu");
         for(const item of menu_items) {
-            item.onclick = () => {
-                menu.style.display = "none";
-                hamburger_is_open = false;
-            };
+            item.onclick = close_hamburger_menu;
         }
 
     });
@@ -133,9 +169,10 @@
     }
 
     .full {
-        display: flex;
-        align-items: center;
-        justify-content: space-around;
+        display: grid;
+        grid-template-columns: 1fr auto 1fr;
+        grid-template-rows: 1fr;
+        
         height: 116px;
     }
 
@@ -154,23 +191,56 @@
             flex-direction: column;
         }
 
-        .nav {
+        .hamburger {
+            grid-column: 1;
+            grid-row: 1;
+            
             display: flex;
+            align-items: center;
+            justify-content: center;
+
+        }
+
+        .logo-container {
+            grid-column: 2;
+            grid-row: 1
+        }
+
+        .language {
+            grid-column: 3;
+            grid-row: 1;
+
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            visibility: hidden;
+        }
+
+        .nav {
+            display: grid;
+            grid-template-columns: 1fr auto 1fr;
+            grid-template-rows: 1fr;
+
             flex-direction: row;
             align-items: center;
             justify-content: space-around;
+            
             height: 81px;
+            width: 100%;
         }
 
         .menu {
             font-family: open-sans;
             font-weight: bold;
+            font-size: 20px;
 
             display: none;
             flex-direction: column;
             align-items: center;
             justify-content: center;
             gap: 30px;
+
+            padding-bottom: 30px;
         }
 
         .logo-name {
