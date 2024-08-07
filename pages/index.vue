04588e51d2363a2569aeb65bccb08b96a6cfbf2e
tomascabrerizo <tomascabrerizo97@gmail.com>
2024-08-07 13:35:31 -0300

    contacto form

--- pages/index.vue
+++ pages/index.vue
@@ -29,13 +29,20 @@
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae maxime asperiores deserunt, velit nobis, autem dolorem accusamus quasi eaque reiciendis laboriosam exercitationem molestias placeat eius sed ipsum! Laudantium temporibus sint officia quod esse nam, cupiditate voluptatibus labore, accusantium earum nihil?</p>
             </div>
         </div>
-
     </section>
     <section class="padded-image">
 
     </section>
     <section class="contacto">
-
+        <form action="">
+            <div class="contacto-form">
+                <input class="contacto-input" type='text' placeholder='Nombre Completo'>
+                <input class="contacto-input" type='text' placeholder='Correo electónico'>
+                <input class="contacto-input" type='text' placeholder='Telefono'>
+                <textarea class="contacto-input" placeholder="Mensaje"></textarea>
+            </div>
+            <input class="contacto-button" type="button" value="Enviar">
+        </form>
     </section>
 </template>
 
@@ -43,6 +50,86 @@
 </script>
 
 <style scoped>
+
+    .contacto {
+        width: 100%;
+        background-color: #ededed;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        padding-top: 87px;
+        padding-bottom: 87px;
+    }
+
+    .contacto form {
+        width: 1338px;
+        height: 575px;
+
+        background-color: #ffffff;
+        border-radius: 15px;
+
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .contacto .contacto-form {
+        
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 1fr 1fr 6fr;
+        gap: 11px;
+        
+        height: 464px;
+        margin: 40px;
+    }
+    
+    .contacto-input:nth-child(1) {
+        grid-column: 1 / span 2;
+        grid-row: 1;
+    }
+
+    .contacto-input:nth-child(2) {
+        grid-column: 1;
+        grid-row: 2;
+    }
+
+    .contacto-input:nth-child(3) {
+        grid-column: 2;
+        grid-row: 2;
+    }
+    
+    .contacto-input:nth-child(4) {
+        grid-column: 1 / span 2;
+        grid-row: 3;
+    }
+
+    .contacto-input {
+        border-radius: 15px;
+        border: solid #B4B4B4 1px;
+        box-sizing: border-box;
+    }
+
+    .contacto div {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+    }
+
+    .contacto .contacto-button {
+        width: 395px;
+        height: 77px;
+        background-color: #BF0000;
+        border-radius: 15px;
+        border: none;
+        align-self: flex-end;
+
+        margin-bottom: 55px;
+        margin-right: 40px
+    }
 
     .cards {
         width: 100%;
@@ -55,7 +142,6 @@
 
         gap: 87px;
         padding-top: 87px;
-        padding-bottom: 87px;
     }
 
     .card {
