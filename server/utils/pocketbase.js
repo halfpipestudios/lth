ed0a0315c70af9298cb9cea7d8240ca239833e13
tomascabrerizo <tomascabrerizo97@gmail.com>
2024-08-20 21:43:03 -0300

    add server ip to pocketbase

--- server/utils/pocketbase.js
+++ server/utils/pocketbase.js
@@ -1,6 +1,7 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase('http://pocketbase:8090');
+//const pb = new PocketBase('http://pocketbase:8090');
 //const pb = new PocketBase('http://127.0.0.1:8090');
+const pb = new PocketBase('http://154.38.163.72:8090');
 
 export default pb;
