925e08772d4148908dedde00462fff68b0f674bf
tomascabrerizo <tomascabrerizo97@gmail.com>
2024-08-21 17:58:07 -0300

    add https to pocketbase

--- server/utils/pocketbase.js
+++ server/utils/pocketbase.js
@@ -2,6 +2,7 @@ import PocketBase from 'pocketbase';
 
 //const pb = new PocketBase('http://pocketbase:8090');
 //const pb = new PocketBase('http://127.0.0.1:8090');
-const pb = new PocketBase('http://154.38.163.72:8090');
+//const pb = new PocketBase('http://154.38.163.72:8090');
+const pb = new PocketBase('https://lth.halfpipe.dev/admin');
 
 export default pb;
