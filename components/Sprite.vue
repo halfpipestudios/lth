ed5146bae61587ec9dc27860864306e0c5503d58
tomascabrerizo <tomascabrerizo97@gmail.com>
2024-09-03 12:10:23 -0300

    fix animations

--- components/Sprite.vue
+++ components/Sprite.vue
@@ -1,7 +1,9 @@
 <template>
 
     <div class="sprite" ref=sprite>
-        <img v-for="(frame, index) in frames" class="frame" :src="frame" alt="sprite frame" ref="frames_element">
+        <img class="frame" :src="frames[current_frame]" alt="sprite frame" ref="frame">
+
+        <!-- <img v-for="(frame, index) in frames" class="frame" :src="frame" alt="sprite frame" ref="frames_element"> -->
     </div>
 
 </template>
@@ -18,61 +20,40 @@
         }
     });
 
-    const frames_element = ref(null);
     const sprite = ref(null);
-    let current_frame = 0;
+    const frame = ref(null);
+    const current_frame = ref(0);
     let animation_is_pause = true;
+    let interval_id = 0;
 
     function start() {
-        if(!frames_element.value) return;
-        if(!frames_element.value.length) return;
-
+        current_frame.value = 0;
         animation_is_pause = false;
-        if(current_frame !== 0 && frames_element.value[current_frame].style.display === "block") {
-            frames_element.value[current_frame].style.display = "none";
-        }
-        current_frame = 0;
-        frames_element.value[current_frame].style.display = "block";
-
-    }
-
-    function pause() {
-        if(!frames_element.value) return;
-        if(!frames_element.value.length) return;
-
-
-        animation_is_pause = true;
-        frames_element.value[current_frame].style.display = "block";
     }
 
     function update() {
-    
-        if(!frames_element.value) return;
-        if(frames_element.value.length === 0) return;
+        
+        if(!frame.value) return;
         if(animation_is_pause) return;
 
-        frames_element.value[current_frame].style.display = "none";    
-        current_frame = (current_frame + 1) % frames_element.value.length;
-        frames_element.value[current_frame].style.display = "block";
-
-        if(current_frame === (frames_element.value.length - 1)) {
-            pause();
-        }        
+        if(current_frame.value === (props.frames.length - 1)) {
+            animation_is_pause = true;
+        } else {
+            current_frame.value = (current_frame.value + 1);
+        }
+    
     }
-
-    let interval_id = 0;
 
     function handle_intersection(entries, observer) {
 
         entries.forEach(function (entry) {
             if(entry.isIntersecting === true) {
                 start();
-                interval_id = setInterval(update, props.frame_time * 1000);
+                interval_id = setInterval(update, props.frame_time * 1000)
             } else {
-                pause();
-                if(interval_id) clearInterval(interval_id);
+                clearInterval(interval_id);
             }
-        });
+        }); 
 
     }
 
@@ -108,8 +89,6 @@
 
     width: 100%;
     height: 100%;
-
-    display: none;
 }
 
 </style>
