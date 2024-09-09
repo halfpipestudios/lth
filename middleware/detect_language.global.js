38aced836ff64175d2bceb765f56138eb1ee08b8
tomascabrerizo <tomascabrerizo97@gmail.com>
2024-09-09 12:16:44 -0300

    save language (not finished)

--- middleware/detect_language.global.js
+++ middleware/detect_language.global.js
@@ -1,17 +1,21 @@
 export default defineNuxtRouteMiddleware((to, from) => {
+
     if (import.meta.server) {
+
+        let lang_ext = "en";
+
         const nuxtApp = useNuxtApp()
         const event = nuxtApp.ssrContext?.event
         if (event) {
             const acceptLanguage = event.node.req.headers['accept-language']
-            let lang_ext = "en";
             if (acceptLanguage) {
                 const preferredLanguage = acceptLanguage.split(',')[0];
                 lang_ext = preferredLanguage.toString().split('-')[0];
             }
-            useState('language', () => lang_ext)
-        } else {
-            useState('language', () => "en")
         }
+
+        useState('language', () => lang_ext)
+
     }
+
 });
