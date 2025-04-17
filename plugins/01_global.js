99a6297dc822d147b75ee7729b7ec6231b89af6f
tomascabrerizo <tomascabrerizo97@gmail.com>
2025-04-17 18:33:45 -0300

    improve language selection

--- plugins/01_global.js
+++ plugins/01_global.js
@@ -1,11 +1,19 @@
 export default defineNuxtPlugin(async (nuxtApp) => {
 
+
+    let lang_ext = "en";
+    const event =  useNuxtApp().ssrContext?.event
+    if (event) {
+        const acceptLanguage = event.node.req.headers['accept-language']
+        if (acceptLanguage) {
+            const preferredLanguage = acceptLanguage.split(',')[0];
+            lang_ext = preferredLanguage.toString().split('-')[0];
+        }
+    }
+
     const { data: repo } = await useAsyncData('language-repo', () => $fetch("/api/texts"));
-    const language = useState('language');
-    if(!language.value) {
-        language.value = "es"
-    } 
     const language_repo = useState('laguage-repo', () => repo);
+    const language = useState('language', () => lang_ext);
 
     function update_texts_language() {
         let texts = {};
